769237d7a4300fbd8342f6c0aed4a2dbd8b2eb7a
Mark Tsai <mavin@mail.com>
2022-03-29 17:53:35 +0800

    220329 commit

--- src/addClassRecord.js
+++ src/addClassRecord.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 
 import Select from 'react-select';
@@ -10,6 +11,10 @@ import Select from 'react-select';
 
         const [members, setMembers] = useState([]);
         const [lessons, setLessons] = useState([]);
+
+        const [member, setMember] = useState('');
+        const [lesson, setLesson] = useState('');
+        const [date, setDate] = useState('');
 
         useEffect(() => {
             fetch("http://localhost:8080/members")
@@ -31,6 +36,15 @@ import Select from 'react-select';
         let member_options = members.map((i) => ({ label: i.member_name, value: i.member_name }));
         let lesson_options = lessons.map((i) => ({ label: i.lesson_name, value: i.lesson_name }));
 
+        const handleSumit = async (e) => {
+          e.preventDefault();
+          await axios.post('http://localhost:8080/addNewRecord',{
+            member: member,
+            lesson: lesson,
+            date: date
+          });
+      }
+
 
         return (
           <div>
@@ -38,11 +52,10 @@ import Select from 'react-select';
               新增上課紀錄
             </h1>
 
-            <form className="form" >
-                選擇學員姓名 : <Select options={member_options} />
-                選擇課程 : <Select options={lesson_options} />
-                選擇上課日 : <input className='payDay' type="date" ></input>
-
+            <form className="form" onSubmit={ handleSumit }>
+                選擇學員姓名 : <Select options={member_options} onChange={ (e) => setMember(e.target.value)} />
+                選擇課程 : <Select options={lesson_options} onChange={ (e) => setLesson(e.target.value)} />
+                選擇上課日 : <input className='payDay' type="date" onChange={ (e) => setDate(e.target.value) }></input>
                 <Button >送出上課紀錄</Button>
            </form>
 
